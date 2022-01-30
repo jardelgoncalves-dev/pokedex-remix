290b4ff46baa69dd7764f45460083aec9f1e48a7
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-01-30 15:05:04 -0300

    feature: import fonts and type colors

--- app/root.tsx
+++ app/root.tsx
@@ -10,13 +10,19 @@ import {
 import type { MetaFunction } from "remix";
 
 import globalStyle from '~/styles/global.css'
+import typeStyle from '~/styles/type-colors.css'
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "Pokedex" };
 };
 
 export const links: LinksFunction = () => [
-  { href: globalStyle, rel: 'stylesheet' }
+  { href: "https://fonts.googleapis.com", rel: "preconnect" },
+  { href: "https://fonts.gstatic.com", rel: "preconnect", crossOrigin: "anonymous" },
+  { href: "https://fonts.gstatic.com", rel: "preconnect", crossOrigin: "anonymous" },
+  { href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=VT323&display=swap", rel: "stylesheet" },
+  { href: globalStyle, rel: 'stylesheet' },
+  { href: typeStyle, rel: 'stylesheet' },
 ]
 
 export default function App() {
